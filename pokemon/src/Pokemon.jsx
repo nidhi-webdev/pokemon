e7d3ec44fe2914b6bea77b7d62a71bdb7b0a85ca
nidhi <kumarinidhi0594@gmail.com>
2026-02-14 18:07:49 +0100

    Getting the data on the console

--- pokemon/src/Pokemon.jsx
+++ pokemon/src/Pokemon.jsx
@@ -1,14 +1,37 @@
+import { useState, useEffect } from "react"
 
 const Pokemon = () => {
+    // const [PokemonData, setPokemonData] = useState("")
+
+
+    const PokemonApi = "https://pokeapi.co/api/v2/pokemon"
+
+    const fetchApi = async () => {
+        try {
+            const res = await fetch(PokemonApi)
+            const data = await res.json()
+            console.log("there is data from Pokemon", data);
+
+        } catch (error) {
+            console.log(error)
+
+        }
+    }
+
+    useEffect(() => {
+        fetchApi()
+    }, [])
+
+
     return (
         // <div className="bg-red-300 min-h-screen">
-            <div className="flex flex-col items-center bg-blue-50 min-h-screen ">
-                <h1 className="font-extrabold text-3xl mt-10"> Let's Catch Pokemon </h1>
+        <div className="flex flex-col items-center bg-blue-50 min-h-screen ">
+            <h1 className="font-extrabold text-3xl mt-10"> Let's Catch Pokemon </h1>
 
-                <div>
-                    <input type="text" placeholder="Search Pokemon" className="px-4 py-3 border-b-2 border-black pt-7 outline-0"/>
-                </div>
+            <div>
+                <input type="text" placeholder="Search Pokemon" className="px-4 py-3 border-b-2 border-black pt-7 outline-0" />
             </div>
+        </div>
         // </div>
 
     )
