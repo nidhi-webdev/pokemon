0fd2dcef03513d2997ce14625ceb18b35d4c1db3
nidhi <kumarinidhi0594@gmail.com>
2026-02-14 12:38:49 +0100

    Tailwind

--- pokemon/src/Pokemon.jsx
+++ pokemon/src/Pokemon.jsx
@@ -3,10 +3,10 @@ const Pokemon = () => {
     return (
         // <div className="bg-red-300 min-h-screen">
             <div className="flex flex-col items-center bg-blue-50 min-h-screen ">
-                <h1 className="font-extrabold text-3xl"> Let's Catch Pokemon </h1>
+                <h1 className="font-extrabold text-3xl mt-10"> Let's Catch Pokemon </h1>
 
                 <div>
-                    <input type="text" placeholder="Search Pokemon" />
+                    <input type="text" placeholder="Search Pokemon" className="px-4 py-3 border-b-2 border-black pt-7 outline-0"/>
                 </div>
             </div>
         // </div>
