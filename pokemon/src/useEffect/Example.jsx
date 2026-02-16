c9636df167f0696b754cce0c63fc229bed81e262
nidhi <kumarinidhi0594@gmail.com>
2026-02-16 18:05:03 +0100

    desing

--- pokemon/src/useEffect/Example.jsx
+++ pokemon/src/useEffect/Example.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const UserefExample = () => {
-  return (
-    <div>
-      Hello Use Effect
-    </div>
-  )
+    return (
+        <div className='bg-blue-950 min-h-screen p-8 text-center'>
+           <h1 className='text-white text-4xl font-bold'>Hello Use Effect</h1> 
+        </div>
+    )
 }
 
 export default UserefExample
