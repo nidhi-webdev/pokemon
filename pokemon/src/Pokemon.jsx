135daae469605b37a51dedacd5e587a1a3bd4ebb
nidhi <kumarinidhi0594@gmail.com>
2026-02-16 17:49:25 +0100

    Search functionality

--- pokemon/src/Pokemon.jsx
+++ pokemon/src/Pokemon.jsx
@@ -5,6 +5,7 @@ const Pokemon = () => {
     const [pokemonData, setPokemonData] = useState([])
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null)
+    const [search, setSearch] = useState("")
 
     const PokemonApi = "https://pokeapi.co/api/v2/pokemon?limit=124"
 
@@ -43,19 +44,22 @@ const Pokemon = () => {
         return <h1 className="flex justify-center items-center font-bold text-3xl min-h-screen"> {error.message} </h1>
     }
 
+    const searchData = pokemonData.filter((curPokemon) => curPokemon.name.toLowerCase().includes(search.toLowerCase()))
+
 
     return (
         <div className="flex flex-col items-center bg-blue-50 min-h-screen ">
             <h1 className="font-extrabold text-3xl mt-10"> Let's Catch Pokemon </h1>
 
             <div>
-                <input type="text" placeholder="Search Pokemon" className="px-4 py-3 border-b-2 border-black pt-7 outline-0" />
+                <input value={search} onChange={(e) => setSearch(e.target.value)}
+                    type="text" placeholder="Search Pokemon" className="px-4 py-3 border-b-2 border-black pt-7 outline-0" />
             </div>
 
             <div>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-8">
                     {
-                        pokemonData.map((curPokemon) => {
+                        searchData.map((curPokemon) => {
                             return <PokemonCards key={curPokemon.id} curPokemon={curPokemon} />
                         })
                     }
