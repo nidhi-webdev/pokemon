dbf035505779d57ff6dc6fb40bed8624d4737873
nidhi <kumarinidhi0594@gmail.com>
2026-02-16 16:47:05 +0100

    detail

--- pokemon/src/PokemonCards.jsx
+++ pokemon/src/PokemonCards.jsx
@@ -9,14 +9,12 @@ const PokemonCards = ({ curPokemon }) => {
                 alt={curPokemon.name}
                 className="w-24 h-24 object-contain mb-2" />
             <h2 className="text-xl font-bold capitalize mb-1">{curPokemon.name}</h2>
-            <button className='bg-green-500 px-3 py-3 rounded-4xl text-white'> {curPokemon.types.map((curTypes) => curTypes.type.name).join(',')}</button>
-            <div>
-                <span> Height  {curPokemon.height} </span>
-                <span> Weight  {curPokemon.weight} </span>
-                <span> Speed  {curPokemon.stats[5].base_stat} </span>
+            <button className='bg-green-500 px-3 py-3 rounded-4xl text-white mt-5 cursor-pointer'> {curPokemon.types.map((curTypes) => curTypes.type.name).join(',')}</button>
+            <div className='mt-4 flex justify-between w-full text-sm px-2 gap-1'>
+                <span className="whitespace-nowrap"><b>Height:</b> {curPokemon.height}</span>
+                <span className="whitespace-nowrap"><b>Weight:</b> {curPokemon.weight}</span>
+                <span className="whitespace-nowrap"><b>Speed:</b> {curPokemon.stats[5].base_stat}</span>
             </div>
-
-
         </div>
     )
 }
