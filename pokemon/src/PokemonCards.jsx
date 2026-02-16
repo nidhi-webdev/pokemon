5ec83b1787c325b102f1f2c964fedcf7d28f1418
nidhi <kumarinidhi0594@gmail.com>
2026-02-16 17:24:22 +0100

    Other information

--- pokemon/src/PokemonCards.jsx
+++ pokemon/src/PokemonCards.jsx
@@ -9,11 +9,17 @@ const PokemonCards = ({ curPokemon }) => {
                 alt={curPokemon.name}
                 className="w-24 h-24 object-contain mb-2" />
             <h2 className="text-xl font-bold capitalize mb-1">{curPokemon.name}</h2>
-            <button className='bg-green-500 px-3 py-3 rounded-4xl text-white mt-5 cursor-pointer'> {curPokemon.types.map((curTypes) => curTypes.type.name).join(',')}</button>
+            <button className='bg-green-500 px-3 py-3 rounded-2xl text-white mt-5 cursor-pointer'> {curPokemon.types.map((curTypes) => curTypes.type.name).join(',')}</button>
             <div className='mt-4 flex justify-between w-full text-sm px-2 gap-1'>
                 <span className="whitespace-nowrap"><b>Height:</b> {curPokemon.height}</span>
                 <span className="whitespace-nowrap"><b>Weight:</b> {curPokemon.weight}</span>
                 <span className="whitespace-nowrap"><b>Speed:</b> {curPokemon.stats[5].base_stat}</span>
+            </div>
+
+            <div className='mt-4 flex justify-between w-full text-sm px-2 gap-1'>
+                <span className="whitespace-nowrap"> <b>Experience:</b> {curPokemon.base_experience}</span>
+                <span className="whitespace-nowrap"> <b>Attack:</b> {curPokemon?.stats[1]?.base_stat}</span>
+                <span className="whitespace-nowrap"> <b>Speed:</b> {curPokemon.stats[5].base_stat}</span>
             </div>
         </div>
     )
