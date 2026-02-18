cb921395a1705e691b486761aaf7a714e58d37f8
nidhi <kumarinidhi0594@gmail.com>
2026-02-18 21:25:06 +0100

    It provides the Id automatically

--- pokemon/src/useId/UserId.jsx
+++ pokemon/src/useId/UserId.jsx
@@ -1,18 +1,25 @@
+import { useId } from "react"
+
+
 
 const UserId = () => {
-  return (
-    <div className='p-10'>
-      <form className='flex flex-col items-center min-h-screen gap-7'>
-        <label htmlFor='username'> Username  </label>
-        <input id='username' type='text' className='border border-black p-2 rounded-xl'/>
+    const usernameId = useId()
+    const emailId = useId()
 
-        <label htmlFor='email'> Email </label>
-        <input id='email' type='email' className='border border-black p-2 rounded-xl' />
 
-        <button className='bg-green-700 text-white px-3 py-2 rounded-xl cursor-pointer' > Submit   </button>
-      </form>
-    </div>
-  )
+    return (
+        <div className='p-10'>
+            <form className='flex flex-col items-center min-h-screen gap-7'>
+                <label htmlFor={usernameId}> Username  </label>
+                <input id= {usernameId} type='text' name="username" className='border border-black p-2 rounded-xl' />
+
+                <label htmlFor={emailId}> Email </label>
+                <input id={emailId} type='email' name="email" className='border border-black p-2 rounded-xl' />
+
+                <button className='bg-green-700 text-white px-3 py-2 rounded-xl cursor-pointer' > Submit   </button>
+            </form>
+        </div>
+    )
 }
 
 export default UserId
