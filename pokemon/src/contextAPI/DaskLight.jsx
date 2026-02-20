e1c3bb2d8f081c8edddb8f7862ee845c9a855fb5
nidhi <kumarinidhi0594@gmail.com>
2026-02-20 21:04:10 +0100

    Condition on tailwind

--- pokemon/src/contextAPI/DaskLight.jsx
+++ pokemon/src/contextAPI/DaskLight.jsx
@@ -27,7 +27,9 @@ export const DarkLightComp = () => {
     const { theme, handleToggleTheme } = use(ThemeContext)
 
 
-    return <div className="p-4 flex flex-col justify-center items-center min-h-screen gap-5">
+    return <div className={`p-4 flex flex-col justify-center items-center min-h-screen gap-5 
+    ${theme === "dark" ? "bg-gray-800" : "bg-white"}`}>
+
         <h1 className="font-extrabold text-4xl"> Dark Light Mode Website </h1>
         <p className="font-bold text-2xl"> Hello !! My React v19 Fans </p>
         <button className="bg-blue-500 rounded px-4 py-2 hover:bg-blue-600 cursor-pointer"
