e509298d25ea070f1438b7820a4e379a38d070b0
nidhi <kumarinidhi0594@gmail.com>
2026-02-20 20:54:29 +0100

    taiwind

--- pokemon/src/contextAPI/DaskLight.jsx
+++ pokemon/src/contextAPI/DaskLight.jsx
@@ -27,9 +27,12 @@ export const DarkLightComp = () => {
     const { theme, handleToggleTheme } = use(ThemeContext)
 
 
-    return <div>
-        <h1> Dark Light Mode Website </h1>
-        <p> Hello !! My React v19 Fans </p>
-        <button onClick={handleToggleTheme}> {theme === "dark" ? "Switch to Light Mode" : "Switch to Dark Mode"} </button>
+    return <div className="p-4 flex flex-col justify-center items-center min-h-screen">
+        <h1 className="font-extrabold text-4xl"> Dark Light Mode Website </h1>
+        <p className="font-bold text-2xl"> Hello !! My React v19 Fans </p>
+        <button className=""
+            onClick={handleToggleTheme}>
+            {theme === "dark" ? "Switch to Light Mode" : "Switch to Dark Mode"}
+        </button>
     </div>
 }
