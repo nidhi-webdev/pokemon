7f5fefc7a7c0647e247cf26592045d9afcaa50df
nidhi <kumarinidhi0594@gmail.com>
2026-02-21 12:49:59 +0100

    useReducer Hook, This is more organise version of Usestate,previously we were we were using the usetstate something like that

---
+++ pokemon/src/useReducer/UseReducer.jsx
@@ -0,0 +1,10 @@
+
+export const UseReducer = () => {
+const [count, setCount] = uCeState(0)
+
+    return <div>
+        <h1></h1>
+        <button> Increment </button>
+        <button> Decrement </button>
+    </div>
+}
