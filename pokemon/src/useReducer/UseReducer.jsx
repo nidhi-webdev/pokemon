6fe25bccf34823a723a3718da1bcbe18c55948d2
nidhi <kumarinidhi0594@gmail.com>
2026-02-21 13:07:42 +0100

    Dispath funtion

--- pokemon/src/useReducer/UseReducer.jsx
+++ pokemon/src/useReducer/UseReducer.jsx
@@ -1,15 +1,19 @@
 import { useReducer } from "react"
 
 export const UseReducer = () => {
-    const reducer = (state, action ) => {
+    const reducer = (state, action) => {
+        console.log(state, action);
+        
 
     }
 
     const [count, dispatch] = useReducer(reducer, 0)
 
     return <div className="flex flex-col justify-center items-center p-10 gap-4">
-        <h1></h1>
-        <button className="bg-amber-600 px-4 py-2 rounded cursor-pointer"> Increment </button>
-        <button className="bg-green-600 px-4 py-2 rounded cursor-pointer"> Decrement </button>
+        <h1> {count} </h1>
+        <button onClick={() => dispatch({ type: "INCREMENT"})}
+            className="bg-amber-600 px-4 py-2 rounded cursor-pointer"> Increment </button>
+        <button onClick={() => dispatch({type: "DECREMENT"})}
+            className="bg-green-600 px-4 py-2 rounded cursor-pointer"> Decrement </button>
     </div>
 }
