a4a1f4b6a6870faa9f97137a163a70cac179c18e
nidhi <kumarinidhi0594@gmail.com>
2026-02-21 13:09:32 +0100

    reducer function

--- pokemon/src/useReducer/UseReducer.jsx
+++ pokemon/src/useReducer/UseReducer.jsx
@@ -3,6 +3,13 @@ import { useReducer } from "react"
 export const UseReducer = () => {
     const reducer = (state, action) => {
         console.log(state, action);
+
+        if(action.type === "INCREMENT") {
+            return state + 1 
+        } 
+        if(action.type === "DECREMENT") {
+            return state - 1 
+        }
         
 
     }
@@ -10,7 +17,7 @@ export const UseReducer = () => {
     const [count, dispatch] = useReducer(reducer, 0)
 
     return <div className="flex flex-col justify-center items-center p-10 gap-4">
-        <h1> {count} </h1>
+        <h1 className="font-extrabold text-5xl"> {count} </h1>
         <button onClick={() => dispatch({ type: "INCREMENT"})}
             className="bg-amber-600 px-4 py-2 rounded cursor-pointer"> Increment </button>
         <button onClick={() => dispatch({type: "DECREMENT"})}
