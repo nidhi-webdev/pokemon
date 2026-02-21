84749019c081a4dc7162bfa5f556506ed923a701
nidhi <kumarinidhi0594@gmail.com>
2026-02-21 12:56:31 +0100

    Design

--- pokemon/src/useReducer/UseReducer.jsx
+++ pokemon/src/useReducer/UseReducer.jsx
@@ -7,9 +7,9 @@ export const UseReducer = () => {
 
     const [count, dispatch] = useReducer(reducer, 0)
 
-    return <div className="flex flex-col justify-center min-h-screen">
+    return <div className="flex flex-col justify-center items-center p-10 gap-4">
         <h1></h1>
-        <button> Increment </button>
-        <button> Decrement </button>
+        <button className="bg-amber-600 px-4 py-2 rounded cursor-pointer"> Increment </button>
+        <button className="bg-green-600 px-4 py-2 rounded cursor-pointer"> Decrement </button>
     </div>
 }
