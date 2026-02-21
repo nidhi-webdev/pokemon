f70364a8918dc3115e9e1c21d51dc360d7e93cc8
nidhi <kumarinidhi0594@gmail.com>
2026-02-21 13:30:53 +0100

    Use Reducer Best practice

--- pokemon/src/useReducer/UseReducer.jsx
+++ pokemon/src/useReducer/UseReducer.jsx
@@ -1,26 +1,34 @@
 import { useReducer } from "react"
 
 export const UseReducer = () => {
-    const reducer = (state, action) => {
-        console.log(state, action);
+const initialState = {
+    count: 0
+}
 
-        if (action.type === "INCREMENT") {
-            return state + 1
-        }
-        if (action.type === "DECREMENT") {
-            return state - 1
-        }
-        if(action.type === "RESET") {
-            return (state = 0)
+    const reducer = (state, action) => {
+        switch(action.type) {
+            case "INCREMENT":
+            return {count: state.count + 1 }
+
+            case "DECREMENT":
+            return {count: state.count - 1 }
+
+            case "RESET": 
+            return {count: 0 }
+
+            default: 
+            return state
+
+
         }
 
 
     }
 
-    const [count, dispatch] = useReducer(reducer, 0)
+    const [state, dispatch] = useReducer(reducer, initialState)
 
     return <div className="flex flex-col justify-center items-center p-10 gap-4">
-        <h1 className="font-extrabold text-5xl"> {count} </h1>
+        <h1 className="font-extrabold text-5xl"> {state.count} </h1>
         <button onClick={() => dispatch({ type: "INCREMENT" })}
             className="bg-amber-600 px-4 py-2 rounded cursor-pointer text-white"> Increment </button>
         <button onClick={() => dispatch({ type: "DECREMENT" })}
