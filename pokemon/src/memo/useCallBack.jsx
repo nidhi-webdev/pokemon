0d5adc72ac90f90485e1dae3fcdf51c304dd6fe8
nidhi <kumarinidhi0594@gmail.com>
2026-02-24 17:24:05 +0100

    function defination

--- pokemon/src/memo/useCallBack.jsx
+++ pokemon/src/memo/useCallBack.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react"
+
 export const UseCallBack = () => {
+    const [count, setCount] = useState(0)
+
+    const increment = () => {
+        console.log("Increment Inside");
+        setCount((prev) => prev + 1)
+
+    }
+
+    const decrement = () => {
+        console.log("")
+    }
+
     return (
         <div className="p-4 flex flex-col justify-center items-center h-1vh tracking-wider gap-4">
-            <h1 className="text-5xl font-extrabold"> Count:  </h1>
-            <button className="bg-green-800 px-4 py-2 rounded"> Increment </button>
-            <button className="bg-red-800 px-4 py-2 rounded"> Decrement </button>
+            <h1 className="text-5xl font-extrabold"> Count: {count} </h1>
+            <button onClick={increment}
+                className="bg-green-800 px-4 py-2 rounded"> Increment </button>
+            <button onClick={decrement}
+                className="bg-red-800 px-4 py-2 rounded"> Decrement </button>
         </div>
     )
 }
