4bddf566988a4dfec06463e3ea6e3cdaa1552e05
nidhi <kumarinidhi0594@gmail.com>
2026-02-24 16:28:38 +0100

    Tailwind

--- pokemon/src/memo/useCallBack.jsx
+++ pokemon/src/memo/useCallBack.jsx
@@ -1,9 +1,9 @@
 export const UseCallBack = () => {
     return (
-        <div className="">
-            <h1> Count:  </h1>
-            <button> Increment </button>
-            <button> Decrement </button>
+        <div className="p-4 flex flex-col justify-center items-center h-1vh tracking-wider gap-4">
+            <h1 className="text-5xl font-extrabold"> Count:  </h1>
+            <button className="bg-green-800 px-4 py-2 rounded"> Increment </button>
+            <button className="bg-red-800 px-4 py-2 rounded"> Decrement </button>
         </div>
     )
 }
