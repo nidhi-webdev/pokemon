90c8167790ed58797715d749fead896c4c05e361
nidhi <kumarinidhi0594@gmail.com>
2026-02-24 17:48:16 +0100

    Use memo but also it is not working but in the Youtube it is working

--- pokemon/src/memo/useCallBack.jsx
+++ pokemon/src/memo/useCallBack.jsx
@@ -1,14 +1,17 @@
-import { useState } from "react"
+import { useState, memo } from "react"
 
 
-const Button = ({ onClick, children }) => {
+const Button = memo(({ onClick, children }) => {
+console.log(`Rendering Button: ${children}`)
+
+
     return (
-        <button className={`px-4 py-2 rounded
+        <button className={`px-4 py-2 rounded cursor-pointer text-white
             ${children.trim() === "Increment" ? "bg-green-800" : "bg-red-800"} `}
             onClick={onClick}> {children}
         </button>
     )
-}
+})
 
 
 
@@ -33,9 +36,9 @@ export const UseCallBack = () => {
         <div className="p-4 flex flex-col justify-center items-center h-1vh tracking-wider gap-4">
             <h1 className="text-5xl font-extrabold"> Count: {count} </h1>
             <Button onClick={increment} > Increment </Button>
-               
+
             <Button onClick={decrement}> Decrement </Button>
-                
+
         </div>
     )
 }
