a408b96c501f1fa0b9d90089050c293f8e8ab949
nidhi <kumarinidhi0594@gmail.com>
2026-02-24 17:40:39 +0100

    the condition

--- pokemon/src/memo/useCallBack.jsx
+++ pokemon/src/memo/useCallBack.jsx
@@ -1,5 +1,21 @@
 import { useState } from "react"
 
+
+const Button = ({ onClick, children }) => {
+    return (
+        <button className={`px-4 py-2 rounded
+            ${children.trim() === "Increment" ? "bg-green-800" : "bg-red-800"} `}
+            onClick={onClick}> {children}
+        </button>
+    )
+}
+
+
+
+
+
+
+
 export const UseCallBack = () => {
     const [count, setCount] = useState(0)
 
@@ -10,16 +26,16 @@ export const UseCallBack = () => {
     }
 
     const decrement = () => {
-        console.log("")
+        setCount((prev) => prev - 1)
     }
 
     return (
         <div className="p-4 flex flex-col justify-center items-center h-1vh tracking-wider gap-4">
             <h1 className="text-5xl font-extrabold"> Count: {count} </h1>
-            <button onClick={increment}
-                className="bg-green-800 px-4 py-2 rounded"> Increment </button>
-            <button onClick={decrement}
-                className="bg-red-800 px-4 py-2 rounded"> Decrement </button>
+            <Button onClick={increment} > Increment </Button>
+               
+            <Button onClick={decrement}> Decrement </Button>
+                
         </div>
     )
 }
